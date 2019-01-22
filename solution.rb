9b06642b8c2408bc74a573340856cb5271bb1836
wilmer142 <wilmercollege@gmail.com>
2019-01-21 22:43:21 -0500

    Reto 6

--- solution.rb
+++ solution.rb
@@ -7,6 +7,6 @@ end
 get '/respuesta' do
 	frase = params[:frase] ? params[:frase] == params[:frase].upcase ? "Ahhh si, manzanas!" : "Habla más duro mijito" : "Habla más duro mijito"
 	<<-HTML
-		<h1><%= frase %></h1>
+		<h1>#{frase}</h1>
 	HTML
 end
